625a7daceeba773176969e0e95a9f00f3ff35acd
Trent <mr.peschke@gmail.com>
2019-01-06 14:17:48 -0700

    added fake auth

--- server/server.js
+++ server/server.js
@@ -5,28 +5,37 @@ const express = require('express')
     , CronJob = require('cron').CronJob
     , { server, connection, auth } = require('./serv-config')
     , ctrl = require('./controller')
+    , session = require('express-session')
+    , passport = require('passport')
 
 const app = new express()
 app.use(bodyParser.json())
 app.use(cors())
+app.use(session({
+    secret: auth,
+    resave: false,
+    saveUninitialized: true
+}))
+app.use(passport.initialize());
+app.use(passport.session());
 
 ///////////////////////////////////
 ////TESTING TOPLEVEL MIDDLEWARE////
 ///COMMENET OUT WHEN AUTH0 READY///
 ///////////////////////////////////
-// app.use((req, res, next) =>{
-//     if(!req.session.user){
-//         req.session.user = {
-//             user_id: 1,
-//             user_name: "harrison ford", 
-//             email: "adventureBuilder2049@gmail.com", 
-//             name: "adventure", 
-//             profile_picture : "http://www.placekitten.com/200/250",
-//             auth_id: "adsgfhaoibjmoi5wrhgiuaosfngiuasdhg;ioarhdgv;ou"
-//         }
-//     }
-//     next();
-// })
+app.use((req, res, next) =>{
+    if(!req.session.user){
+        req.session.user = {
+            user_id: 1,
+            user_name: "harrison ford", 
+            email: "adventureBuilder2049@gmail.com", 
+            name: "adventure", 
+            profile_picture : "http://www.placekitten.com/200/250",
+            auth_id: "adsgfhaoibjmoi5wrhgiuaosfngiuasdhg;ioarhdgv;ou"
+        }
+    }
+    next();
+})
 
 // ===============================
 
