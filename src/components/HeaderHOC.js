5b595bf5352bb66078e2c0e04bc2083a1bc276c3
Trent <mr.peschke@gmail.com>
2019-01-09 12:01:58 -0700

    did mobile stlying on header and home

--- src/components/HeaderHOC.js
+++ src/components/HeaderHOC.js
@@ -4,9 +4,11 @@ export default function HeaderHOC(Page) {
     return props =>
         <div>
             <div className="headerShell">
-                <div className="headerImage"></div>
-                <h2>A Spell and Miracle reference for the <a href="http://highadventuregames.net">Bonfire Roleplaying Game</a></h2>
-                <i className={props.match.path === '/' ? "hidden" : "fa fa-home home"} onClick={_=>props.history.push('/')}></i>
+                <div className="mobileFlex">
+                    <div className="headerImage"></div>
+                </div>
+                <h2 className="mobileFix">A Spell and Miracle reference for the <a href="http://highadventuregames.net">Bonfire Roleplaying Game</a></h2>
+                <i className={props.match.path === '/' ? "hidden" : "fa fa-home home"} onClick={_ => props.history.push('/')}></i>
             </div>
 
             <Page {...props} />
